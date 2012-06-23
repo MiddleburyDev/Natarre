11461aca4ec1608ab1573cb6265fe33bae917ff2
Will Potter <will@wpotter.me>
2012-06-23 16:16:09 -0400

    a bit of functionality

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,8 +1,26 @@
 
 class StoriesController < ApplicationController
 
+	def index
+		@stories = Story.all(:order => "created_at DESC")
+	end
+
 	def show
 		@story = Story.find_by_id(params[:id])
+	end
+
+	def new
+		@story = Story.new
+
+	end
+
+	def create
+		@story = Story.new params[:story]
+		if @story.save
+			redirect_to :root
+		else
+
+		end
 
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Natarre::Application.routes.draw do
 
-
+  get "stories/new"
+  post "stories/create"
+  get "stories" => "stories#index", :as => :stories_index
+  post "stories/create" => "stories#create", :as => :stories
+  get "stories/new" => "stories#new", :as => :new_story
   get "story/:id" => "stories#show"
 
   get "register" => "users#register", :as => :register
