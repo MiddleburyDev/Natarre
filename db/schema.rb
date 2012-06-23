d77af3f29e0ff17f88c8cf21369ccda837454c21
Teddy Knox <teddyknox@gmail.com>
2012-06-23 16:04:45 -0400

    hello world

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623182257) do
+ActiveRecord::Schema.define(:version => 20120623194550) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(:version => 20120623182257) do
     t.integer  "story_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "sc_id"
   end
 
   create_table "users", :force => true do |t|
