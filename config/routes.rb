8b85c2ba8b08c50901258c1a14194834fee8fe35
Teddy Knox <teddyknox@gmail.com>
2012-06-23 19:44:08 -0400

    actual merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
 Natarre::Application.routes.draw do
 
-<<<<<<< HEAD
-  get "mobile_api/register"
-
-  get "mobile_api/login"
-
-  get "mobile_api/fb_login"
-
-  get "mobile_api/twitter_login"
-
-  get "mobile_api/upload"
-
-=======
   get "stories/new"
   post "stories/create"
 
   get "prompts" => "stories#prompts", :as => :prompts
   get "popular" => "stories#popular", :as => :popular
   get "stories" => "stories#index", :as => :stories_index
->>>>>>> 86dc18f9882fb411ceab733ee76a428cc16d0faa
   post "stories/create" => "stories#create", :as => :stories
   get "stories" => "stories#index", :as => :stories_index  
   get "stories/new" => "stories#new", :as => :new_story
