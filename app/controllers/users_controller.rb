7367f1582f890effa59c9d581029eb85ef9f446c
Will Potter <will@wpotter.me>
2012-06-23 15:22:59 -0400

    registration is up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
 
-	def show
+	## Shows the register form.
+	def register
+		@user = User.new
+
+	end
+
+	def login
 
 
 	end
 
+	def create
+		@user = User.new params[:user]
+		if @user.save
+			redirect_to :root_path
+		else
 
-
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Natarre::Application.routes.draw do
-  get "home/index"
+
 
   get "story/:id" => "stories#show"
+
+  get "register" => "users#register", :as => :register
+  match "create" => "users#create", :as => :users
+
+
 
   root :to => "home#index"
 
